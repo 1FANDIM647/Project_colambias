a1e4a24a1ed72ad7f9d37298ae30233cbb2b76ed
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-01-20 21:09:47 +0300

    Add files via upload

---
+++ Blessed Home 1.0/Source code/tools.cpp
@@ -0,0 +1,44 @@
+ /* TOOLS FOR BLESSED HOME 1.0
+  Developed by Shishov Michael
+  Email:fandim16k@gmail.com
+ */
+ #include <iostream.h>
+// include library for
+
+ using namespace std;
+
+enum objects_of_blessed_home {
+
+
+
+ };
+ /* main function for work with door
+ in BLESSED HOME  */
+// we got access for job with door
+void  locker_in_home (bool main_door ;) {
+  // function of opennig door
+     void open_door (get.detector = true ) {
+            bool detector = true;
+            while ( detector = true) {
+                button_detector=true;
+            }
+     }
+    //function of closing door
+    void close_door (bool  main_door ; )
+    {
+      if ( bool main_door = false  ) {
+
+        int detector ; // variable for getting signal
+        // we are getting signal
+       if ( int detector = get.detector; ){
+         for (i = 0 , i<1 ,i++ ) {
+           int locker1 ();//closing first locker in door
+         }
+          for ( i = 0; i < 1; i++) {
+            int locker2();//closing second locker in door
+          }
+          for (i = 0 ; i <1 ; i++){
+            int locker3();//closing third locker in door
+          }
+       }
+              
