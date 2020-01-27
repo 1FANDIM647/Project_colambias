2399bee8334a4e58333c0acf399bddfd9968ab18
Olan georgeson <60299218+OlanSand1200@users.noreply.github.com>
2020-01-27 20:47:55 +0300

    Add files via upload

---
+++ Blessed Home 1.0/Main_block_for_work_of_microcontrolers.c
@@ -0,0 +1,26 @@
+/* Program for control of microcontrolers  */
+
+#include <"stdlib.h">
+#include <"math.h">
+#include <"string">
+#include <"assert.h">
+
+/* microcontroler for work in conditioner */
+struct  microcontroler_conditioner {
+
+  string name;
+  int registers;
+
+  // unservice registers
+  string  name_of_registers [] = {"AA1","AA2","AA3","AA4","AA5","AA6","AA7","AA8","AA9","AA10","BB1","BB2","BB3","BB4","BB5","BB6","BB7","BB8","BB9","BB10"} ;
+   
+
+
+};
+
+
+
+int  main(int argc, char const *argv[]) {
+
+  return 0;
+}
