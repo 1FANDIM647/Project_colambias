a7dab46957f1186d087b52ab6481e061be674acc
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-01-30 21:58:43 +0300

    Add files via upload

---
+++ Blessed Home 1.0/light_controller.c
@@ -0,0 +1,16 @@
+/*LIGHT CONTROLLER */
+
+#include "maincontroller.h"
+#include <"stdlib.h">
+#include <"math.h">
+#include <"string">
+#include <"assert.h">
+
+
+int int main(int argc, char const *argv[])
+{  
+	//assembler code 
+int  work_light_controller();
+
+	return 0;
+}
---
+++ Blessed Home 1.0/water_controller.c
@@ -0,0 +1,16 @@
+/*WATER CONTROLLER */
+
+#include "maincontroller.h"
+#include <"stdlib.h">
+#include <"math.h">
+#include <"string">
+#include <"assert.h">
+
+
+int int main(int argc, char const *argv[])
+{  
+	//assembler code 
+int  water_light_controller();
+
+	return 0;
+}
